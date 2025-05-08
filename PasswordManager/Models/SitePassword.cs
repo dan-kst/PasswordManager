d174a841b1148347a886a66917d82d03c0b30839
dan-kst <77903535+dan-kst@users.noreply.github.com>
2025-05-09 01:32:43 +0300

    Rebase.

--- PasswordManager/Models/SitePassword.cs
+++ PasswordManager/Models/SitePassword.cs
@@ -4,13 +4,18 @@ namespace PasswordManager.Models
 {
     public class SitePassword : ISecretable
     {
+        private const int MIN_NAME_LENGTH = 4;
+        private const int MAX_NAME_LENGTH = 100;
+
+        private const int MIN_PASSWORD_LENGTH = 8;
+        private const int MAX_PASSWORD_LENGTH = 20;
         public int Id { get; set; }
         public int IClientId { get; set; }
         [Required]
-        [Range(4, 100, ErrorMessage = "Invalid Name length.")]
+        [Range(MIN_NAME_LENGTH, MAX_NAME_LENGTH, ErrorMessage = "Invalid Name length.")]
         public string Name { get; set; }
         [Required]
-        [Range(8, 20, ErrorMessage = "Invalid Password length.")]
+        [Range(MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH, ErrorMessage = "Invalid Password length.")]
         public string Value { get; set; }
         public EnumSecretQuality SecretQuality { get; set; }
         public DateTime CreatedDate { get; set; }
