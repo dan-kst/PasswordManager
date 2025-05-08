82f40974676ce79bec044d32e0cdc5f08b89d461
dan-kst <77903535+dan-kst@users.noreply.github.com>
2025-05-09 00:24:51 +0300

    Initial Create.

--- PasswordManager/Models/SitePassword.cs
+++ PasswordManager/Models/SitePassword.cs
@@ -1,10 +1,16 @@
-﻿namespace PasswordManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PasswordManager.Models
 {
     public class SitePassword : ISecretable
     {
         public int Id { get; set; }
         public int ClientId { get; set; }
+        [Required]
+        [Range(4, 100, ErrorMessage = "Invalid Name length.")]
         public string Name { get; set; }
+        [Required]
+        [Range(8, 20, ErrorMessage = "Invalid Password length.")]
         public string Value { get; set; }
         public EnumSecretQuality SecretQuality { get; set; }
         public DateTime CreatedDate { get; set; }
--- PasswordManager/Models/User.cs
+++ PasswordManager/Models/User.cs
@@ -1,10 +1,24 @@
-﻿namespace PasswordManager.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PasswordManager.Models
 {
     public class User : IClient
     {
+        private const int MIN_NAME_LENGTH = 4;
+        private const int MAX_NAME_LENGTH = 100;
+
+        private const int MIN_PASSWORD_LENGTH = 8;
+        private const int MAX_PASSWORD_LENGTH = 20;
+
         public int Id { get; set; }
+        [Required]
+        [Range(MIN_NAME_LENGTH, MAX_NAME_LENGTH, ErrorMessage = "Invalid Name length.")]
         public string Name { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
         public string Email { get; set; }
+        [Required]
+        [Range(MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH, ErrorMessage = "Invalid Password length.")]
         public string MasterPassword { get; set; }
         public List<ISecretable>? Secrets { get; set; }
         public User()
