59821b078d09a0b7e6dea1e400bdbd35024f8b76
dan-kst <77903535+dan-kst@users.noreply.github.com>
2025-05-09 03:11:50 +0300

    Initial Create.

---
+++ PasswordManager/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PasswordManager.Contexts;
+
+namespace PasswordManager.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ClientContext _context;
+
+        public AccountController(ClientContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult LoginAccount()
+        {
+            return View();
+        }
+        public IActionResult RegisterAccount()
+        {
+            return View();
+        }
+        public IActionResult EditAccount()
+        {
+            return View();
+        }
+        public IActionResult DeleteAccount()
+        {
+            return View();
+        }
+    }
+}
---
+++ PasswordManager/Controllers/PasswordsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PasswordManager.Controllers
+{
+    public class PasswordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CreatePassword()
+        {
+            return View();
+        }
+        public IActionResult EditPassword()
+        {
+            return View();
+        }
+        public IActionResult DeletePassword()
+        {
+            return View();
+        }
+        public IActionResult ViewPassword()
+        {
+            return View();
+        }
+        public IActionResult SearchPassword()
+        {
+            return View();
+        }
+    }
+}
